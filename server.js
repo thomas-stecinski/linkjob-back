1eb6fa73fa807f3a4ef5702904e8a121f3411c79
Thomas <123959293+thomas-stecinski@users.noreply.github.com>
2024-11-22 16:29:50 +0100

    modif server.js + creation fichier router

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const router = require('./routes/router'); 
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', router);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
